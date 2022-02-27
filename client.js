496438b4c7d71b00125511c0ff7c68df520e8d1e
Raphael Caires <75398550+raphaelcaires@users.noreply.github.com>
2022-02-26 21:42:21 -0300

    websocket

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabaseClient = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  'https://ghgfhvhjfjtrmfqytdwv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzQ5MTI3NywiZXhwIjoxOTU5MDY3Mjc3fQ.00PBa-VXwbWgKI7lT0WGi7ftbqjcGIhX8LHP2HhvAD8'
 )
